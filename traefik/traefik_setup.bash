3983751561e9c978169e1f62abb532c323e951c9
bigboss248 <aminjamali79@gmail.com>
2024-10-08 11:43:52 +0330

    Made the script unix format

--- traefik/traefik_setup.bash
+++ traefik/traefik_setup.bash
@@ -1,5 +1,6 @@
+#!/bin/bash
 sudo iptables -I INPUT -p tcp -m multiport --dports 80,443 -j ACCEPT
-sudo touch acme.json
+sudo touch ./acme.json
 sudo chmod 600 acme.json
-sudo touch acme_cloudflare.json
+sudo touch ./acme_cloudflare.json
 sudo chmod 600 acme_cloudflare.json
