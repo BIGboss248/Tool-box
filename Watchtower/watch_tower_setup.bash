63c67bf8b1622f7225ce54514dc266fc5c0a7bc1
bigboss248 <aminjamali79@gmail.com>
2024-10-12 19:55:00 +0330

    Changed watchtower deployment to docker compose

--- Watchtower/watch_tower_setup.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo bash ./../docker/docker_setup_v2.bash
-docker run --detach \
-    --name watchtower \
-    --volume /var/run/docker.sock:/var/run/docker.sock \
-    containrrr/watchtower
