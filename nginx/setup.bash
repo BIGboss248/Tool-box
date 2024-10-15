c7c1c5cd473776b1c33c1259bf8a9c21dc26eeb8
bigboss248 <aminjamali79@gmail.com>
2024-10-15 13:16:11 +0330

    Deleted nginx pagespeed

--- nginx/setup.bash
+++ nginx/setup.bash
@@ -2,4 +2,4 @@
 # Change Domain.com to domain name
 DOMAIN=example.com
 sudo iptables -I INPUT -p tcp -m multiport --dports 80,443 -j ACCEPT
-sudo docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ -d $DOMAIN
+sudo docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ -d $DOMAIN
