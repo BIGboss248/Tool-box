3b274685c2a5e925fcc0bebc8feba1fbd23a1907
BIGboss248 <aminjamali79@gmail.com>
2024-10-18 21:02:56 +0330

    Linted

--- Cloudflare_tunnel/Cloudflare_tunnel.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Cloudflared download links
-# https://developers.cloudflare.com/cloudflare-one/connections/connect-networks/downloads/#linux
--- Hiddify/Hiddify_Docker_install.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git clone https://github.com/hiddify/hiddify-config
-cd hiddify-config || exit
-touch hiddify-panel/hiddifypanel.db
-docker compose up -d
