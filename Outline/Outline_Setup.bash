3ac9151121073fcc1185f6bcb2605397001cd5fd
BIGboss248 <aminjamali79@gmail.com>
2025-06-16 17:04:50 +0330

    Changed Outline install script to Makefile

--- Outline/Outline_Setup.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Install docker
-echo "Install docker first"
-echo "Remember To save API key"
-# Install script
-sudo bash -c "$(wget -qO- https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/server_manager/install_scripts/install_server.sh)"
