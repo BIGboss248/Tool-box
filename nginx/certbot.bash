bf349c07ecc12f9a344084d7eedf51730a4e5670
Amin Jamali <aminjamali79@gmail.com>
2026-07-28 11:28:03 +0330

    UPdate certificate script

--- nginx/certbot.bash
+++ nginx/certbot.bash
@@ -1,10 +1,12 @@
 #!/bin/bash
-sudo apt update
-sudo apt install snapd
+sudo apt update -y
+sudo apt install snapd -y
 sudo iptables -I INPUT -p tcp -m multiport --dports 80,443 -j ACCEPT
 sudo apt install iptables-persistent
 sudo bash -c iptables-save | sudo tee /etc/iptables/rules.v4 >/dev/null
 sudo netfilter-persistent save
 sudo snap install --classic certbot
 sudo ln -s /snap/bin/certbot /usr/bin/certbot
-sudo certbot certonly --standalone --config-dir ./ssl
+# 2. Get Certificate using Certbot
+echo "Obtaining SSL certificate for $DOMAIN..."
+sudo certbot certonly --standalone -d "$DOMAIN" --register-unsafely-without-email --agree-tos
